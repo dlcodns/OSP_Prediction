7edff6d1f98b3a77c4b9c5cb95d2f033f8092e9b
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2023-04-12 11:32:26 +0900

    uid를 primary key에 추가, uid를 auto_increment 설정함

--- Table1_userAccount.sql
+++ Table1_userAccount.sql
@@ -2,14 +2,12 @@ CREATE DATABASE prediction;	#prediction DB 생성
 
 USE prediction;
 
-create table userAccount (		#name, id, password 컬럼 생성
+create table userAccount (		#UID, name, user_id, password컬럼 생성, primary key= uid, user_id 등록
+	UID INT AUTO_INCREMENT,		#알아서 증가하는 uid 생성
 	name CHAR(8) NOT NULL,
-    id CHAR(10) NOT NULL,
-    password VARCHAR(15) NOT NULL,
-    PRIMARY KEY (id)			#id는 유저끼리 같으면 안되므로 primary key 설정
+    user_id CHAR(10) NOT NULL,
+    password VARCHAR(15) NOT NULL,	#password를 변경하도록 가변형 데이터 사용
+    PRIMARY KEY (UID, user_id)		#id는 유저끼리 같으면 안되므로 primary key 설정
 );
-
-ALTER TABLE `userAccount` CHANGE `id` `user_id` CHAR(10) NOT NULL; 	#id->user_id로 이름 변경
-ALTER TABLE `userAccount` ADD `UID` INT(5) NOT NULL FIRST;		#맨앞에 새로운 컬럼 UID 추가
 
 DESC userAccount;		#Table1: UID  name  user_id  password
