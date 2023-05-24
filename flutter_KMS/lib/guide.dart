480ece837a2ea804744d1a3b85190fd270d2c670
KIMSSI22 <107696062+KIMSSI22@users.noreply.github.com>
2023-05-24 13:38:01 +0900

    Feat: MyApp 클래스 (#32)

--- flutter_KMS/lib/guide.dart
+++ flutter_KMS/lib/guide.dart
@@ -3,3 +3,13 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'guide',
+      home: Guide(title: 'guide'),
+    );
+  }
+}
