7e17995374340068ae606ba5127ab71da9303e7f
KIMSSI22 <107696062+KIMSSI22@users.noreply.github.com>
2023-05-28 04:54:38 +0900

    Update main.dart

--- flutter_KMS/lib/main.dart
+++ flutter_KMS/lib/main.dart
@@ -13,50 +13,48 @@ class MyApp extends StatelessWidget {
       title: 'main',
       home: Scaffold(
         backgroundColor: const Color(0xffE3DCFF),
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(100),
-          child: AppBar(
-            backgroundColor: const Color(0xffE3DCFF),
-            elevation: 0,
-            title: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 30.0, top: 50),
-                  child: Image.asset(
-                    'assets/logo.png',
-                    width: 184,
-                    height: 36,
-                  ),
+        appBar: AppBar(
+          toolbarHeight: 100,
+          backgroundColor: const Color(0xffE3DCFF),
+          elevation: 0,
+          title: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 30.0, top: 50),
+                child: Image.asset(
+                  'assets/logo.png',
+                  width: 184,
+                  height: 36,
                 ),
-                const Spacer(),
-                Padding(
-                  padding: const EdgeInsets.only(right: 12.0, top: 50),
-                  child: IconButton(
-                    icon: const Icon(
-                      Icons.settings_backup_restore,
-                      color: Color(0xff404040),
-                      size: 30,
-                    ),
-                    onPressed: () {
+              ),
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.only(right: 12.0, top: 45),
+                child: IconButton(
+                  icon: const Icon(
+                    Icons.settings_backup_restore,
+                    color: Color(0xff404040),
+                    size: 30,
+                  ),
+                  onPressed: () {
 
-                    },
-                  ),
+                  },
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 30.0, top: 50),
-                  child: IconButton(
-                    icon: const Icon(
-                      Icons.person,
-                      color: Color(0xff404040),
-                      size: 30,
-                    ),
-                    onPressed: () {
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 30.0, top: 45),
+                child: IconButton(
+                  icon: const Icon(
+                    Icons.person,
+                    color: Color(0xff404040),
+                    size: 30,
+                  ),
+                  onPressed: () {
 
-                    },
-                  ),
+                  },
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
