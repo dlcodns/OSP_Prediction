08c78b5cbc573bd644efc7e4ee467f86d71d28b9
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2023-05-30 03:10:05 +0900

    Myapp class  생성

--- flutter_LCU/01_start.dart
+++ flutter_LCU/01_start.dart
@@ -3,3 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());  //앱 시작해주세요!, MyApp(메인페이지 주소)
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+
+    
+  }
+}
