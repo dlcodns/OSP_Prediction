ffe199b7df57502806e5264d95a4a49f07432a7a
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2023-05-30 13:29:17 +0900

    Appbar 꾸미기

--- flutter_LCU/10_modify.dart
+++ flutter_LCU/10_modify.dart
@@ -9,5 +9,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    return MaterialApp(
+      title: 'result',
+      home: Scaffold(
+        backgroundColor: const Color(0xffFFFFFF),
+        appBar: AppBar(
+          toolbarHeight: 100,
+          elevation: 4,
+          shadowColor: Color(0xffE6E6E6),
+          backgroundColor: Color(0xffFFFFFF),
+          title: Padding(
+            padding: EdgeInsets.only(right:12.0,top:43),
+            child: Row(
+              children: const [
+                Icon(Icons.settings, color: Color(0xff404040), size: 30,              ),
+                Text('  설정',
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Color(0xff404040),
+                      fontWeight: FontWeight.bold
+                  ),)
+              ],
+            ),
+          ),
+        ),
+        body: HomeApp(),
+      ),
+    );
   }
 }
