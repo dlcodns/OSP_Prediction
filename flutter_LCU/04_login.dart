42c41219bfb6e7521ea615c06aa01201caa46936
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2023-05-30 13:18:29 +0900

    app바 생성

--- flutter_LCU/04_login.dart
+++ flutter_LCU/04_login.dart
@@ -9,5 +9,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     
+    return MaterialApp(
+      title: 'login',
+      home: Scaffold(
+        backgroundColor: const Color(0xffFFFFFF),
+        appBar: AppBar(
+          toolbarHeight: 100,
+          elevation: 0,
+          shadowColor: Color(0xffE6E6E6),
+          backgroundColor: Color(0xffFFFFFF),
+          title: Padding(
+            padding: EdgeInsets.only(right:12.0,top:43),
+            child: Row(
+              children:  [
+                IconButton(
+                    iconSize: 30,
+                    onPressed: (){
+                      //start_select
+                    },
+                    icon: Icon(Icons.keyboard_backspace,color: Color(0xff404040),)
+                ),
+                Text('  로그인',
+                  style: TextStyle(
+                      fontSize: 28,
+                      color: Color(0xff404040),
+                      fontWeight: FontWeight.bold
+                  ),)
+              ],
+            ),
+          ),
+        ),
+        body: HomeApp(),
+      ),
+    );
   }
 }
