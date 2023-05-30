4f8f9936a6d18097dc0f394b430d5ce97b2e8f9c
KIMSSI22 <107696062+KIMSSI22@users.noreply.github.com>
2023-05-31 05:26:55 +0900

    Feat: upload1 (#32)

--- flutter_KMS/lib/main.dart
+++ flutter_KMS/lib/main.dart
@@ -77,6 +77,60 @@ class Main extends StatelessWidget {
           ),
           Expanded(
             flex: 5,
+            child: Padding(
+              padding: EdgeInsets.only(left: 40, right: 40),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color(0xffF4F1FF),
+                ),
+                child: Column(
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 3,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                '신체 이미지 업로드',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                  color: Color(0xff404040),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: TextButton(
+                                onPressed: () {
+                                  // Handle the button press
+                                },
+                                child: Image.asset(
+                                  'assets/upload.png',
+                                  width: 60,
+                                  height: 60,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: SizedBox(),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
           Expanded(
             flex: 1,
@@ -84,6 +138,7 @@ class Main extends StatelessWidget {
           ),
           Expanded(
             flex: 5,
+           
           ),
           Expanded(
             flex: 2,
@@ -92,10 +147,12 @@ class Main extends StatelessWidget {
           Container(
             padding: EdgeInsets.only(left: 135, right: 135),
             child: ElevatedButton(
+              
             ),
           ),
           Expanded(
             flex: 6,
+           
           ),
           Expanded(
             flex: 4,
