203e10935df5753f13a426ac985f1f9468cb99ea
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2023-05-30 13:21:45 +0900

    homeapp class 추가

--- flutter_LCU/05_celebration.dart
+++ flutter_LCU/05_celebration.dart
@@ -18,3 +18,40 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class HomeApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    var m = MediaQuery.of(context);
+    print("넓이 : ${m.size.width}");
+    print("높이 : ${m.size.height}");
+    return Stack(
+      children: [
+        Column(
+          children: [
+            Flexible(
+              fit: FlexFit.loose,
+              child: Align(
+                  alignment: const Alignment(0.0, 0.0),
+                  child: Image.asset('5_cele.png',
+                      width: MediaQuery.of(context).size.width*0.75)
+              ),
+            ),
+            Align(
+              alignment: Alignment(0.0, 0.7),
+              child: Text('아무 곳이나 클릭하세요',
+                  style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xffAFA2E5)
+                  ),
+                  textAlign: TextAlign.center
+              ),
+            ),
+            Container(height: 60),
+          ],
+        ),
+      ],
+    );
+  }
+}
