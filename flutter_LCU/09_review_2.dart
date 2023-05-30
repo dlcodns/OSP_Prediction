edf76a10703e457de4b1f8ce9430794f802ad6ef
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2023-05-30 20:00:55 +0900

    Update 09_review_2.dart

--- flutter_LCU/09_review_2.dart
+++ flutter_LCU/09_review_2.dart
@@ -3,3 +3,31 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());  //앱 시작해주세요!, MyApp(메인페이지 주소)
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'review_2',
+      home: Scaffold(
+        backgroundColor: const Color(0xffE3DCFF),
+        appBar: AppBar(
+          backgroundColor: Color(0xffE3DCFF),
+          title: Row(
+            children: const [
+              Icon(Icons.settings_backup_restore,color: Color(0xff404040),              ),
+              Text('  다시 보기',
+                style: TextStyle(
+                    fontSize: 24,
+                    color: Color(0xff404040),
+                    fontWeight: FontWeight.bold
+                ),)
+            ],
+          ),
+        ),
+        body: HomeApp(),
+      ),
+    );
+  }
+}
