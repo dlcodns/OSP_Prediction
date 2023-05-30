927a3a5e7f2b7a8069e007e2fcbb08336a7860e2
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2023-05-30 22:18:13 +0900

    Update main.dart

--- result/lib/main.dart
+++ result/lib/main.dart
@@ -1,19 +1,39 @@
-//result 페이지입니다
 import 'package:flutter/material.dart';
+import 'package:result/review_2.dart';
 
 void main() {
-  runApp(const MyApp());  //앱 시작해주세요!, MyApp(메인페이지 주소)
+  runApp(const ReviewOne());  //앱 시작해주세요!, MyApp(메인페이지 주소)
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class ReviewOne extends StatelessWidget {
+  const ReviewOne({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
 
     return MaterialApp(
-      title: 'result',
+      title: 'review_1',
       home: Scaffold(
         backgroundColor: const Color(0xffE3DCFF),
+        appBar: AppBar(
+          toolbarHeight: 100,
+          elevation: 4,
+          shadowColor: Color(0xffE6E6E6),
+          backgroundColor: Color(0xffE3DCFF),
+          title: Padding(
+            padding: EdgeInsets.only(right:12.0,top:43),
+            child: Row(
+              children: const [
+                Icon(Icons.settings_backup_restore, color: Color(0xff404040), size: 30,              ),
+                Text('  다시 보기',
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Color(0xff404040),
+                      fontWeight: FontWeight.bold
+                  ),)
+              ],
+            ),
+          ),
+        ),
         body: HomeApp(),
       ),
     );
@@ -26,102 +46,111 @@ class HomeApp extends StatelessWidget{
     var m = MediaQuery.of(context);
     print("넓이 : ${m.size.width}");
     print("높이 : ${m.size.height}");
-    return Center(
-      child: Stack(
-          children: <Widget>[
-            Column(
-              children: [
-                Expanded(
-                  flex: 8,
-                  child: Align(
-                    alignment: Alignment(0.0,0.5),
-                    child: Image.asset(
-                      '2_Fitting_Room.png',
-                      width: MediaQuery.of(context).size.width * 0.5,
+    return Column(
+      children: [
+        Flexible(
+          flex: 1,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                  children:[
+                    SizedBox(width: MediaQuery.of(context).size.width*0.02),
+                    IconButton(
+                        padding: EdgeInsets.zero,
+                        onPressed: (){
+                          //home
+                        },
+                        icon: const Icon(Icons.keyboard_backspace,
+                            color: Color(0xffFFFFFF),
+                            size: 40
+                        )
+                    ),
+                  ]
+              ),
+              Align(
+                alignment: Alignment(0.0,0.8),
+                child: Row(
+                  children: [
+                    Icon(Icons.view_agenda,
+                        color: Color(0xffFFFFFF),
+                        size: 20),
+                    IconButton(
+                        padding: EdgeInsets.zero,
+                        constraints: BoxConstraints(),
+                        onPressed: (){
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context)=>ReviewTwo()));
+                        },
+                        icon: const Icon(Icons.dashboard,
+                            color: Color(0xffFFFFFF),
+                            size: 20
+                        )
+                    ),
+                    SizedBox(width: MediaQuery.of(context).size.width*0.02),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Flexible(
+          fit: FlexFit.loose,
+          flex: 12,
+          child: Center(
+            child: Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xffEEEAFF),
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(width:10,color: Color(0xffEEEAFF)
+                      ),
                     ),
                   ),
-                ),
-                Flexible(
-                  fit: FlexFit.loose,
-                  flex: 25,
-                  child: Center(
-                    child: Container(
-                        margin: EdgeInsets.fromLTRB(30, 0, 30, 0),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Color(0xffFFFFFF),
-                          border: Border.all(
-                              color: Color(0xffEEEAFF),
-                              width: 10,
-                              style: BorderStyle.solid
-                          ),
-                        ),
-                        child: Image.asset('miso.jpg',fit: BoxFit.contain)
-                    ),
-                  ),
-                ),
-                Flexible(
-                  flex: 10,
-                  child: Align(
-                    alignment: Alignment(0.0,-0.6),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                  ListView(
                       children: [
-                        Flexible(
-                          fit: FlexFit.loose,
-                          child: SizedBox(
-                              width: MediaQuery.of(context).size.width*0.3,
-                              height: 50,
-                              child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: const Color(0xffA490F8),),
-                                child: const Text('저장',
-                                  style: TextStyle(
-                                      fontSize: 30,
-                                      fontWeight: FontWeight.bold
-                                  ),
-                                ),
-                                onPressed: () {
-                                  //갤러리에 저장
-                                },
-                              )
-                          ),
-                        ),
-                        Flexible(
-                          child: SizedBox(width: MediaQuery.of(context).size.width*0.15),
-                        ),
-                        Flexible(
-                          fit: FlexFit.loose,
-                          child: SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.3,
-                            height: 50,
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: const Color(0xffFFFFFF),
+                        Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(width:10,color: Color(0xffEEEAFF)
                               ),
-                              child: const Text(
-                                '메인',
-                                style: TextStyle(
-                                  color: Color(0xffA490F8),
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              onPressed: () {
-                                //메인페이지 이동
-                              },
                             ),
-                          ),
+                            child: Image.asset('miso.jpg',fit: BoxFit.contain)
                         ),
-                      ],
-                    ),
+                        Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(width:10,color: Color(0xffEEEAFF)
+                              ),
+                            ),
+                            child: Image.asset('TY.png',fit: BoxFit.contain)
+                        ),
+                        Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(width:10,color: Color(0xffEEEAFF)
+                              ),
+                            ),
+                            child: Image.asset('kitty.png',fit: BoxFit.contain)
+                        ),
+                        Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(width:10,color: Color(0xffEEEAFF)
+                              ),
+                            ),
+                            child: Image.asset('heart.jpg',fit: BoxFit.contain)
+                        ),
+                      ]
                   ),
-                ),
-              ],
+                ]
             ),
-          ]
-      ),
+          ),
+        ),
+      ],
     );
+
   }
 }
-
