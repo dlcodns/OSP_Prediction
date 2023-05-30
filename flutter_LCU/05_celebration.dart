763b756262505674b71b3f42689d608633dd8799
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2023-05-30 13:20:42 +0900

    Myapp class 생성

--- flutter_LCU/05_celebration.dart
+++ flutter_LCU/05_celebration.dart
@@ -3,3 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());  //앱 시작해주세요!, MyApp(메인페이지 주소)
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'celebration',
+      home: Scaffold(
+        backgroundColor: const Color(0xffE3DCFF),
+        body: HomeApp(),
+      ),
+    );
+  }
+}
