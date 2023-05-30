6b3f82594a4c533f3216aa154c938e631a69dbc4
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2023-05-30 13:31:00 +0900

    homeapp class 추가

--- flutter_LCU/10_modify.dart
+++ flutter_LCU/10_modify.dart
@@ -45,6 +45,94 @@ class HomeApp extends StatelessWidget{
     var m = MediaQuery.of(context);
     print("넓이 : ${m.size.width}");
     print("높이 : ${m.size.height}");
-    
+    return ListView(
+      children: [
+        Expanded(
+          child: Center(
+            child: SizedBox(
+              height: 225,
+              child: Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Color(0xffFFFFFF),
+                ),
+                child: Stack(
+                  alignment: Alignment.bottomRight, // 아이콘을 원의 오른쪽 아래에 위치시키기 위해 설정
+                  children: [
+                    CircleAvatar(
+                      radius: 80,
+                      backgroundColor: Color(0xffEDEDED),
+                      child: Icon(Icons.person, size: 130, color: Color(0xff757575)),
+                    ),
+                    CircleAvatar(
+                      radius: 18,
+                      backgroundColor: Color(0xffFFFFFF),
+                      child: MaterialButton(
+                        onPressed: () {
+                          //수정
+                        },
+                        color: Color(0xff6744F2),
+                        padding: EdgeInsets.all(12),
+                        shape: CircleBorder(),
+                        child: Icon(
+                          Icons.edit,
+                          size: 20,
+                          color: Color(0xffFFFFFF),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const [
+            Flexible(
+              flex: 1,
+              child: SizedBox(),
+            ),
+            Flexible(
+              flex: 40,
+              child: Align(
+                alignment: Alignment(-0.9,0.0),
+                child: Text('이름',
+                  style: TextStyle(
+                      fontSize: 16
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        Container(
+          height: MediaQuery.of(context).size.height*0.01
+        ),
+        Center(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width*0.9,
+            height: MediaQuery.of(context).size.height*0.1,
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Color(0xffEDEDED),
+                  labelText: '이름 db 불러오기',
+                  labelStyle: TextStyle(
+                      color: Colors.red
+                  ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xffEDEDED)),
+                ),
+              ),
+              style: TextStyle(
+                  fontSize: 14),
+            ),
+          ),
+        ),
+      ],
+      );
   }
 }
