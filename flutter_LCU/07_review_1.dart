5d05be09c0e4ac18afec09e052693033fac9172c
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2023-05-30 19:54:22 +0900

    homeapp class 추가

--- flutter_LCU/07_review_1.dart
+++ flutter_LCU/07_review_1.dart
@@ -9,5 +9,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+        return MaterialApp(
+      title: 'review_1',
+      home: Scaffold(
+        backgroundColor: const Color(0xffE3DCFF),
+        appBar: AppBar(
+          backgroundColor: Color(0xffE3DCFF),
+          title: Row(
+            children: const [
+              Icon(Icons.settings_backup_restore,color: Color(0xff404040),              ),
+              Text('  다시 보기',
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Color(0xff404040),
+                  fontWeight: FontWeight.bold
+                ),)
+            ],
+          ),
+        ),
+        body: HomeApp(),
+      ),
+    );
+  }
+}
+
+class HomeApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    var m = MediaQuery.of(context);
+    print("넓이 : ${m.size.width}");
+    print("높이 : ${m.size.height}");
   }
 }
