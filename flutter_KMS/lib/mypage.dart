2f9f84807043c589003e1ba326751906f3c63ddb
KIMSSI22 <107696062+KIMSSI22@users.noreply.github.com>
2023-06-03 05:48:21 +0900

    Feat: appbar (#32)

--- flutter_KMS/lib/mypage.dart
+++ flutter_KMS/lib/mypage.dart
@@ -4,3 +4,38 @@ import 'package:image_picker/image_picker.dart';
 void main() {
   runApp(const MyPage());
 }
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'mypage',
+      home: Scaffold(
+        backgroundColor: const Color(0xffE3DCFF),
+        appBar: AppBar(
+          toolbarHeight: 60,
+          elevation: 4,
+          shadowColor: Color(0xffE6E6E6),
+          backgroundColor: Color(0xffE3DCFF),
+          title: Padding(
+            padding: EdgeInsets.only(right:12.0),
+            child: Row(
+              children: const [
+                Icon(Icons.menu, color: Color(0xff404040), size: 30),
+                Text('  마이페이지',
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Color(0xff404040),
+                      fontWeight: FontWeight.bold
+                  ),)
+              ],
+            ),
+          ),
+        ),
+        body: My(),
+      ),
+    );
+  }
+}
